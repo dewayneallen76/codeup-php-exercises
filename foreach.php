8f979942cb397c720b68fde5f645041996c0d1a7
Dewayne Allen <dewayneallen76@gmail.com>
2016-08-31 13:42:08 -0500

    Construct a loop that iterates through each value and outputs its type as either integer, float, boolean, array, null, or string.

--- foreach.php
+++ foreach.php
@@ -8,10 +8,11 @@ foreach ($things as $value) {
 		echo "{$value} is a number".PHP_EOL;
 	} else if (is_string($value)) {
 		echo "{$value} is a string".PHP_EOL;
-	}
-	 else if (is_array($value)) {
-	 	echo " array is an array".PHP_EOL;
-	 } else if (is_bool($value)) {
+	} else if (is_array($value)) {
+	 	foreach ($value as $value) {
+	 		echo $value . "is an array".PHP_EOL;
+	 	}
+	} else if (is_bool($value)) {
 	 	echo "$value is a boolean".PHP_EOL;
-	 }
+	}
 }
