fbe59a99e1372483c6ea4fd479e43786886a63e2
Dewayne Allen <dewayneallen76@gmail.com>
2016-08-31 13:47:42 -0500

    Construct a loop that iterates through each value and outputs only values with a type that is scalar.

--- foreach.php
+++ foreach.php
@@ -4,15 +4,6 @@
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
 foreach ($things as $value) {
-	if (is_numeric($value)){
-		echo "{$value} is a number".PHP_EOL;
-	} else if (is_string($value)) {
-		echo "{$value} is a string".PHP_EOL;
-	} else if (is_array($value)) {
-	 	foreach ($value as $value) {
-	 		echo $value . "is an array".PHP_EOL;
-	 	}
-	} else if (is_bool($value)) {
-	 	echo "$value is a boolean".PHP_EOL;
-	}
+	if (is_scalar($value))
+		echo "$value is a scalar".PHP_EOL;
 }
