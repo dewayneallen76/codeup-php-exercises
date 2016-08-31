43c65e6b0aa8ecf573d1ed129360e547c0b11a75
Dewayne Allen <dewayneallen76@gmail.com>
2016-08-31 10:06:45 -0500

    Refactor to allow user to choose increment. (count by 1, 2, 10, ...)

--- for.php
+++ for.php
@@ -3,11 +3,12 @@ fwrite(STDOUT, "Enter a starting number: ".PHP_EOL);
 $a = trim(fgets(STDIN));
 fwrite(STDOUT, "Enter an ending number: ".PHP_EOL);
 $b = trim(fgets(STDIN));
-
+fwrite(STDOUT, "Enter an increment amount: ".PHP_EOL);
+$i = trim(fgets(STDIN));
 
 echo PHP_EOL;
 
-for($a = $a; $a <= $b; $a += 1) {
+for($a = $a; $a <= $b; $a += $i) {
   fwrite(STDOUT, "$a".PHP_EOL);
 }
 
