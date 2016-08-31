2f7b97ff615fdf5663421d3144248326ed52552a
Dewayne Allen <dewayneallen76@gmail.com>
2016-08-31 14:25:56 -0500

    REFACTORED AFTER REVIEW TO UNDERSTAND BETTER

--- foreach.php
+++ foreach.php
@@ -5,11 +5,13 @@ $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, 
 
 
 foreach ($things as $thing) {
-	
+	echo "The type is " . gettype($thing) . ". The value is: ";
 	if(is_array($thing)) {
-		echo "Array :".PHP_EOL;
 		foreach($thing as $value) {
-			echo $value. PHP_EOL;
+			echo $value . " ";
 		}
-	} else echo $thing.PHP_EOL;
+	} else {
+ 		echo $thing;
+ 	}
+ 	echo PHP_EOL;
 } 
