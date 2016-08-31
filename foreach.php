6ec809d320fbaccd2b5b2704681cd23ef3add8da
Dewayne Allen <dewayneallen76@gmail.com>
2016-08-31 14:13:18 -0500

    Create a loop that will echo out every value, including those nested in arrays.

--- foreach.php
+++ foreach.php
@@ -3,7 +3,13 @@
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
-foreach ($things as $value) {
-	if (is_scalar($value))
-		echo "$value is a scalar".PHP_EOL;
-}
+
+foreach ($things as $thing) {
+	
+	if(is_array($thing)) {
+		echo "Array :".PHP_EOL;
+		foreach($thing as $value) {
+			echo $value. PHP_EOL;
+		}
+	} else echo $thing.PHP_EOL;
+} 
