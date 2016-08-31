cffaf9a65e22feb43451091f801a8b7c8826b56c
Dewayne Allen <dewayneallen76@gmail.com>
2016-08-31 13:31:39 -0500

    foreach exercises

---
+++ foreach.php
@@ -0,0 +1,17 @@
+<?php 
+
+
+$things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
+
+foreach ($things as $value) {
+	if (is_numeric($value)){
+		echo "{$value} is a number".PHP_EOL;
+	} else if (is_string($value)) {
+		echo "{$value} is a string".PHP_EOL;
+	}
+	 else if (is_array($value)) {
+	 	echo " array is an array".PHP_EOL;
+	 } else if (is_bool($value)) {
+	 	echo "$value is a boolean".PHP_EOL;
+	 }
+}
