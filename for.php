cba8821b1ef142f28a21540122e1b197ec95e116
Dewayne Allen <dewayneallen76@gmail.com>
2016-08-31 11:02:49 -0500

    Make sure you are only allowing users to pass in numbers. Give an error message is both passed arguments are not numeric.

--- for.php
+++ for.php
@@ -6,17 +6,15 @@ $b = trim(fgets(STDIN));
 fwrite(STDOUT, "Enter an increment amount: ".PHP_EOL);
 $i = trim(fgets(STDIN));
 
+$numbersAreValid = is_numeric($a) && is_numeric($b) && is_numeric($i);
+
 echo PHP_EOL;
 
 for($a = $a; $a <= $b; $a += $i) {
-  if($i == 0) {
-    fwrite(STDOUT, $a .PHP_EOL);
-    $a +=1;
-  }
+  fwrite(STDOUT, $a .PHP_EOL);
 }
+ if ($numbersAreValid === false) {
+    echo "Please enter a valid number" .PHP_EOL;
+ }
 
-
-
-
-
-?>
+echo PHP_EOL;
