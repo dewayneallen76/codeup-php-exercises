488437984ae2c1f21cc435120b817aaa2ca1a297
Dewayne Allen <dewayneallen76@gmail.com>
2016-09-01 10:47:37 -0500

    HAD FIRST PART OF EXERCISE WRITTEN BUT NOT ACTUALLY USING THE CONTINUE.

--- break_continue.php
+++ break_continue.php
@@ -1,10 +1,10 @@
 <?php 
 // Create a for loop that shows all even numbers between 1 and 100 using continue.
 for ($i = 1; $i <= 100 ; $i++) {
-	if ($i % 2 == 0) {
-		echo $i .PHP_EOL;
+	if ($i % 2 == 1) {
 		continue;
 	} 
+	echo $i .PHP_EOL;
 }
 
 echo PHP_EOL;
