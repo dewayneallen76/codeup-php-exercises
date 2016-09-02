3ac650c3a8632c81e502bfc7ebc3b4477aa24b09
Dewayne Allen <dewayneallen76@gmail.com>
2016-09-01 21:26:11 -0500

    Sort the companies from "biggest" to "smallest". This may be easier than you think, but be sure you don't loose the company names!

--- technology-companies.php
+++ technology-companies.php
@@ -45,8 +45,7 @@ $companies = [
     ]
 ];
 
-
+// Sort the companies from "biggest" to "smallest". This may be easier than you think, but be sure you don't loose the company names!
+arsort($companies) .PHP_EOL;
 print_r($companies) .PHP_EOL;
-
-
 
