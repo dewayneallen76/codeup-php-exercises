ef37109ea519b7de4b926778959bacfcea6a8029
Dewayne Allen <dewayneallen76@gmail.com>
2016-09-02 12:15:43 -0500

    TRYING TO ADD FUNCTION USING SEARCH ARRAY TO COMPARE, COULD NOT GET IT TO WORK. USED ARRAY_INTERSECT

--- search-arrays.php
+++ search-arrays.php
@@ -17,5 +17,19 @@ searchArray('Bob', $names);
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
 // Create a function to compare 2 arrays that returns the number of values in common between the arrays. Use the 2 example arrays and make sure your solution uses array_search().
-	
+
+// function compareArrays($array1, $array2) {
+// 	$count = 0;
+// 	foreach ($array1 as $value) {
+// 		if (searchArray($array2, $value)) {
+// 			$count += 1;
+// 		}
+// 	}
+// 	return $count;
+// }
+// var_dump(compareArrays($names, $compare));
+
+
+$result = array_intersect($names, $compare); 
+print_r($result);
 
