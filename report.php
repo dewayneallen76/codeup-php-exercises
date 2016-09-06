2fca4256ab366e6d5d7d0d86b226fb7fc8b6b067
Dewayne Allen <dewayneallen76@gmail.com>
2016-09-06 14:54:30 -0500

    ADDED ASSOCIATIVE AREA FOR BETTER DESCRIPTION OF ITEMS. ADDED COMMENTS TO CODE.

--- report.php
+++ report.php
@@ -2,17 +2,29 @@
 
 function salesReport($filename) 
 {
+	// opening file report.txt and making an array 
 	$filename = 'report.txt';
 	$handle = fopen($filename, 'r');
 	$contents = fread($handle, filesize($filename));
-	$reportArray = explode("\n", $contents);
+	$report = explode("\n", $contents);
+	// iteration through array to remove unnecessary information
 	for ($i = 0; $i <= 13; $i++) {
-		array_shift($reportArray);
+		array_shift($report);
 	}
-	
+	foreach ($report as $employee) {
+		$reportArray = explode(",", $employee);
+	// created associative array to display information for each employee 
+		$reportAssociativeArray = [];
+		$reportAssociativeArray["employee number"] = $reportArray[0];
+		$reportAssociativeArray["first name"] = $reportArray[1];
+		$reportAssociativeArray["last name"] = $reportArray[2];
+		$reportAssociativeArray["units sold"] = $reportArray[3];
+		array_push($report, $reportAssociativeArray);
+		array_shift($report);
+	}
 
 	fclose($handle);
-	return($reportArray);
+	return($report);
 }
 
 var_dump(salesReport('report.txt'));
