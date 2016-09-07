3465d8212e5ba4fa17e4cde8f884d96c27dbdca9
Dewayne Allen <dewayneallen76@gmail.com>
2016-09-07 10:59:28 -0500

    FORMATTING CHANGES TO OUTPUT

--- logger.php
+++ logger.php
@@ -3,10 +3,10 @@
 function logMessage($logLevel, $message)
 {
     date_default_timezone_set('America/Chicago');
-    $logDate = date("Y-m-d  h:m:s");
+    $logDate = date("Y-m-d  h:i:s");
     $filename = 'log-YYYY-MM-DD.log';
     $handle = fopen($filename, 'a');
-    $addMessage = $logDate . " " . $message;
+    $addMessage = $logDate . " " . "[$logLevel]" ." " . $message;
     fwrite($handle, $addMessage . PHP_EOL);
     fclose($handle);
   
