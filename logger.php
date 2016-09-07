dceaa2d63498b133d6a35c46459f0c9a99963e95
Dewayne Allen <dewayneallen76@gmail.com>
2016-09-07 11:30:23 -0500

    UPDATED LOGGER.PHP TO APPEND FILE NAME IF DATE IS NOT FOUND TO LOG TO A NEW FILE FOR THAT DATE. ADDED COMMENTS. DELETED UNNESSECARY LOG FILES THAT WERE CREATED AND NO LONGER NEEDED.

--- logger.php
+++ logger.php
@@ -3,15 +3,30 @@
 function logMessage($logLevel, $message)
 {
     date_default_timezone_set('America/Chicago');
+    // variable for adding date to the log output
     $logDate = date("Y-m-d  h:i:s");
-    $filename = 'log-YYYY-MM-DD.log';
+    $filename = date("Y-m-d") . ".log";
     $handle = fopen($filename, 'a');
+    // message that will  be logged to the log file
     $addMessage = $logDate . " " . "[$logLevel]" ." " . $message;
     fwrite($handle, $addMessage . PHP_EOL);
     fclose($handle);
   
     return $addMessage;
 }
+// logMessage("INFO", "This is an info message.");
+// logMessage("ERROR", "This is an error message.");
 
-logMessage("INFO", "This is an info message.");
-logMessage("ERROR", "This is an error message.");
+// Add logInfo() and logError() functions that call logMessage(), passing the appropriate log level values. Replace the calls to logMessage() with calls to the new functions you just created.
+function logError($message) 
+{
+ 	logMessage('ERROR', $message);
+}
+ function logInfo($message) 
+{
+	logMessage('INFO', $message);
+}
+
+logError("Error occured");
+logInfo("User Logged In");
+
