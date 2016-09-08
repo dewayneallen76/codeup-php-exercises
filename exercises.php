ed3ce407336bb004c45133ab087f2b41caab2d53
Dewayne Allen <dewayneallen76@gmail.com>
2016-09-08 09:21:33 -0500

    USED FOR PHP QUIZ

--- exercises.php
+++ exercises.php
@@ -1,7 +1,13 @@
 <?php 
 
-$physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
-$physicistsArray = explode(', ', $physicistsString);
-var_dump($physicistsArray);
-$physicistsString = implode(',', $physicistsArray);
-echo $physicistsString;
+for ($i = 1; $i <= 15 ; $i+) {
+	if ($i / 15 == 0) {
+		echo "FizzBuzz" . PHP_EOL;
+	} else if ($i / 3 == 0) {
+		echo "Fizz" . PHP_EOL;
+	} else if ($i / 5 == 0) {
+		echo "Buzz" . PHP_EOL;
+	} else {
+		echo $i . PHP_EOL;
+	}
+}
