2e9b50e70dcfe749efdec0b8ce966d7e7b3461d2
Dewayne Allen <dewayneallen76@gmail.com>
2016-09-13 15:57:11 -0500

    REFACTORED LOG.PHP TO USE CONSTRUCT AND DESTRUCT MAGIC METHODS. TWO NEW FILES CREATED FROM LOG_TEST.PHP

--- log.php
+++ log.php
@@ -1,17 +1,31 @@
 <?php 
 // Create a file in your exercises directory called Log.php
-// 
 class Log 
 {
 // A property called $filename where you store the name of the file for the log.
 	public $filename;
-// A method called logMessage() that will take in a log level and message as before. It will open the file stored in $filename for appending, output the message in the same format as before, and then close the handle.
+	public $datetime;
+	public $handle;
+// Add a constructor to your Log class.
+// Take in a parameter called $prefix. If nothing is passed to the constructor, the $prefix should default to 'log'.
+	public function __construct($prefix = 'log') 
+	{
+		$this->datetime = date("Y-m-d");
+// Set the $filename property of the class to $prefix-YYYY-MM-DD.log.
+		$this->filename = $prefix . "-". $this->datetime . ".log";
+// Open the $filename for appending and assign the file pointer to the property $handle
+		$this->handle = fopen($this->filename, 'a');
+	}
+// Add a destructor to close $handle when the class is destroyed.
+	public function __destruct() 
+	{
+		fclose($this->handle);
+	}
+// Update logMessage(); it should no longer need to open and close its own file handle, instead use the $handle property.
 	public function logMessage($logLevel,$message) 
 	{
-    	$handle = fopen($this->filename, 'a');
     	$addMessage = date("Y-m-d  h:i:s") . " " . "[$logLevel]" ." " . $message;
-    	fwrite($handle, $addMessage . PHP_EOL);
-    	fclose($handle);
+    	fwrite($this->handle, $addMessage . PHP_EOL);
   
     	return $addMessage;
 	}
--- log_test.php
+++ log_test.php
@@ -4,9 +4,10 @@ date_default_timezone_set('America/Chicago');
 // Create a log_test.php file in your exercises directory. At the top of log_test.php, be sure to require_once 'Log.php';
 require_once 'log.php';
 // Create an instance of the Log class
-$NewInstance = new Log();
+// Update log_test.php; pass the prefix 'cli' to the class and do not manually set the $filename property. Test from the command line.
+$NewInstance = new Log('cli');
 // Set the $filename property in the class; use the format log-YYYY-MM-DD.log.
-$NewInstance->filename = "log-". date("Y-m-d") . ".log";
+// $NewInstance->filename;
 // Call the methods logMessage(), info(), and error() with to test the functionality of each.
 $NewInstance->info("This is an info message.");
 $NewInstance->error("This is an error message.");
