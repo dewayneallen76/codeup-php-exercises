443c430dff0ca93408e5c299566b58d14b94603a
Dewayne Allen <dewayneallen76@gmail.com>
2016-09-28 10:47:55 -0500

    ADDED ECHOS FOR SHOWING RESULTS

--- shapes_test.php
+++ shapes_test.php
@@ -1,11 +1,11 @@
 <?php 
-require_once('rectangle.php');
 require_once('square.php');
 
-$test1 = new Rectangle(3,5);
-echo $test1->area() .PHP_EOL;
-
-$test2 = new Square(3,5);
-echo $test2->perimeter() . PHP_EOL;
+$test1 = new Rectangle(10,5);
+echo "The area is: " . $test1->area() .PHP_EOL;
+echo "The perimeter: " . $test1->perimeter() .PHP_EOL;
+$test2 = new Square(9);
+echo "The perimeter is: " . $test2->perimeter() . PHP_EOL;
+echo "The area is: " .$test2->area() .PHP_EOL;
 
  ?>
