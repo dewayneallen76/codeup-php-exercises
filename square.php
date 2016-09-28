aa132a8d687c5227ff0b9bbb6b2b8cf0ab61d75f
Dewayne Allen <dewayneallen76@gmail.com>
2016-09-28 10:48:19 -0500

    UPDATED CONSTRUCTOR

--- square.php
+++ square.php
@@ -1,10 +1,10 @@
 <?php 
+require_once('rectangle.php');
 
 class Square extends Rectangle 
 {
-	public function __construct($height) {
-		$this->height = $height;
-		$this->width = $height;
+	public function __construct($height) 
+	{
 		parent::__construct($height,$height);
 	}
 
